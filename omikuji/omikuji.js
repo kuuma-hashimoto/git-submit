19d4f258c02783da51f03351f1cae946f6bb64aa
kuuma-hashimoto <137734933+kuuma-hashimoto@users.noreply.github.com>
2023-07-05 15:27:52 +0900

    Update omikuji.js

--- omikuji/omikuji.js
+++ omikuji/omikuji.js
@@ -1,10 +1,10 @@
 let username;
 let userresult;
 username = prompt("お名前を教えてください。")
-document.getElementById("name").innerHTML = username;
 if (username == ""){
     name = "名無し"
 }
+document.getElementById("name").innerHTML = username;
 
 let rand = Math.floor(Math.random()*5) ;
 if (rand == 0) {
